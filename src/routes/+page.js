e1dcc484359f3996a65d84bf27c36542b1990861
clxxiii <earthnoartisjusteh@gmail.com>
2022-09-25 04:35:20 -0400

    try without first page

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -5,8 +5,7 @@ const converter = new Converter({ tables: true, metadata: true });
 export const prerender = true;
 
 export async function load({ url }) {
-  let first = await fetch(`${url.origin}/articles/first.md`);
-  first = await first.text();
-
-  return { page: converter.makeHtml(first) };
+  // let first = await fetch(`${url.origin}/articles/first.md`);
+  // first = await first.text();
+  // return { page: converter.makeHtml(first) };
 }
